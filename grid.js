1fe6e539b0432879c0217efb39adbe7271d99125
amf-mahapatra <aaron.falk.mahapatra@gmail.com>
2022-03-27 11:57:47 -0400

    This is a mess. I only had intermittent time, a few minutes here, few minutes there over the course of two weeks to complete this.

--- grid.js
+++ grid.js
@@ -1,18 +1,41 @@
 
 let container = document.getElementById("grid");
-/*let cell = document.createElement("div");
-cell.setAttribute('id', 'cell');
-cell.textContent = "" */
 let nameCell = ""
 
 
-for (let i = 0; i < 4096; i++) {
-    nameCell = "CellNum" + i
-    cell = document.createElement("div");
-    cell.setAttribute('id', 'cell');
-    cell.setAttribute('class', nameCell);
-    container.appendChild(cell);
-    console.log(i);
+  function buildGrid529() {
+    removeCells();
+    for (let i = 0; i < 529; i++) {
+      nameCell = "CellNum" + i
+      cell = document.createElement("div");
+      cell.setAttribute('id', 'cell23');
+      cell.setAttribute('class', nameCell);
+      cell.setAttribute('name', "EasCell");
+      container.appendChild(cell);
+      console.log(i);
+    } 
+  }
+
+  function buildGrid2116() {
+
+    removeCells();
+    
+    for (let i = 0; i < 2116; i++) {
+      nameCell = "CellNum" + i
+      cell = document.createElement("div");
+      cell.setAttribute('id', 'cell');
+      cell.setAttribute('class', nameCell);
+      cell.setAttribute('name', "EasCell");
+      container.appendChild(cell);
+      console.log(i);
+    } 
+  }
+
+  function removeCells(){
+
+    while (container.hasChildNodes()){
+      container.removeChild(container.firstChild);
+    }
   }
 
 let EaS_Cell = document.getElementById('cell')
@@ -20,7 +43,7 @@ let EaS_Cell = document.getElementById('cell')
 document.addEventListener("mouseover", (e) => {
   let targID = (e.target.id);
 
-  if (targID === 'cell') {
+  if (targID === 'cell23' || targID === 'cell') {
     console.log(e.target.className);
     let moCell = document.getElementsByClassName(`${e.target.className}`);
     console.log(moCell.css);
@@ -31,11 +54,36 @@ document.addEventListener("mouseover", (e) => {
 
 function clearCells() {
   let allCells = document.querySelectorAll("#cell");
+  let allCells23 = document.querySelectorAll("#cell23");
   console.log("clicked clear");
 
   allCells.forEach((c) => {
     c.style.backgroundColor = null;
   })
+
+  allCells23.forEach((g) => {
+    g.style.backgroundColor = null;
+  })
   }
 
+  function Cells23() {
+    let cells = document.getElementsByName('EasCell')
+    console.log(cells.length);
 
+    for (let i = 0; i < cells.length; i++){
+      console.log(cells.item(i));
+      let elem = document.getElementsByClassName(`CellNum${i}`);
+      console.log(elem);
+      elem[0].parentNode.removeChild(elem[0])
+      console.log("this is " + i);
+    }
+  }
+
+  function grid23x(){
+    for (let i = 0; i < 529; i++) {
+      nameCell = "CellNum" + i
+      cell = document.createElement("div");
+      cell.setAttribute('class', nameCell);
+      container.appendChild(cell);
+    }
+  }
